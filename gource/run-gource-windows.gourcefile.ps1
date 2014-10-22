ccf8a45a49174fb479400ea8275695051bc7f48a
Joshua Slik <joshua.b.slik@gmail.com>
2014-10-22 15:46:39 +0200

    Updated gource configurations

--- gource/run-gource-windows.gourcefile.ps1
+++ gource/run-gource-windows.gourcefile.ps1
@@ -1,4 +1,4 @@
 cd ..
-git log --pretty=format:user:%aN%n%ct --reverse --raw --encoding=UTF-8 --no-renames | Out-File -Encoding UTF7 ..\gourcelog.gourcefile.log
-gource ..\gourcelog.gourcefile.log --load-config ./gource/gource-config.gourcefile.ini
+git log --pretty=format:user:%aN%n%ct --reverse --raw --encoding=UTF-8 --no-renames | Out-File -Encoding UTF7 gourcelog.gourcefile.log
+gource gourcelog.gourcefile.log --load-config ./gource/gource-config.gourcefile.ini
 Exit

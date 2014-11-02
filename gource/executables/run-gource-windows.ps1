bbca7c36073a0174c782d632cdefd36b030097c2
Joshua Slik <joshua.b.slik@gmail.com>
2014-11-02 22:59:52 +0100

    Ignored Gource log, forced creation of Gource logging directory

--- gource/executables/run-gource-windows.ps1
+++ gource/executables/run-gource-windows.ps1
@@ -1,6 +1,7 @@
 .\fetchGravatarsForGource.pl
 cd ..
 cd ..
+New-Item -ItemType Directory -Force -Path ./gource/logging | Out-Null
 git log --pretty=format:user:%aN%n%ct --reverse --raw --encoding=UTF-8 --no-renames | Out-File -Encoding UTF7 ./gource/logging/gourcelog.log
 gource ./gource/logging/gourcelog.log --load-config ./gource/config/gource-config.ini
 Exit
